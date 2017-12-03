db477427ab75cfa40d25960e27b1c9bafda093bc
kubili <kubili2013@gmail.com>
2017-12-03 22:33:23 +0800

    utf8mb4 带来的Specified key was too long;问题

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         \Carbon\Carbon::setLocale(\App::getLocale() == 'zh_CN'?'zh':\App::getLocale());
+        Schema::defaultStringLength(128);
     }
 
     /**
